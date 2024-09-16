d950cc9ddbae6a653ff05f42afe7c80082d1fd5a
lokesh <112629714+lokesh96lokesh@users.noreply.github.com>
2024-09-16 15:35:52 +0530

    Add files via upload

--- Addition.java
+++ Addition.java
@@ -1,12 +1,15 @@
-package day1;
+package lms;
 
- class Addition {
-	public static void main(String [] args)
-	{
-		int a=10;
-		int b=5;
-		int c=a+b;
-		System.out.println(c);
+public class Addition {
+	public static void main(String[]args) {
+		String[]rec=args[0].split(" ");
+		int num=Integer.parseInt(args[0]);
+		int num1=Integer.parseInt(args[1]);
+		System.out.println(num);
+		System.out.println(num1);
+		System.out.println(rec[0]);
+		System.out.println(args[1]);
+
 	}
 
 }
